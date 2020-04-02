cdf54c611af7f803e8c5a2660e4b34628ee60d29
Rishav Bhagat <rishavb123@gmail.com>
2020-04-02 18:30:37 -0400

    play train and test scripts

--- play.py
+++ play.py
@@ -103,37 +103,54 @@ class Agent:
 
 if __name__ == '__main__':
     env = gym.make('LunarLander-v2')
-    n_games = 500
-    agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.0005, input_dims=8, n_actions=4, mem_size=1000000, batch_size=64, epsilon_dec=0.999, epsilon_min=0.01)
+    agent = Agent(gamma=0.99, epsilon=0, alpha=0.0005, input_dims=8, n_actions=4, mem_size=1000000, batch_size=64, epsilon_dec=1, epsilon_min=0)
 
-    scores = []
-    eps_history = []
+    agent.load_model()
 
-    agent.load_model('dqn_model.h5')
-
-    for i in range(n_games):
+    while input('Would you like to watch the AI play another game (Y/N)').lower() == 'y':
         done = False
         score = 0
         observation = env.reset()
 
         while not done:
             action = agent.choose_action(observation)
-            next_observation, reward, done, info = env.step(action)
+            observation, reward, done, info = env.step(action)
             score += reward
-            agent.remember(observation, action, reward, next_observation, done)
-            observation = next_observation
-            agent.learn()
+            env.render()
 
-        eps_history.append(agent.epsilon)
-        scores.append(score)
+        print("Score was", score)
 
-        avg_score = np.mean(scores[max(0, i - 100): i + 1])
-        print('episode ', i, 'score %.2f' % score, 'average score %.2f' % avg_score)
+# def train():
+#     env = gym.make('LunarLander-v2')
+#     n_games = 500
+#     agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.0005, input_dims=8, n_actions=4, mem_size=1000000, batch_size=64, epsilon_dec=0.999, epsilon_min=0.01)
 
-        if i % 10 == 0 and i > 0:
-            agent.save_model()
+#     scores = []
+#     eps_history = []
 
-        plt.plot(scores)
-        plt.savefig('Scores.png')
-        plt.plot(eps_history)
-        plt.savefig('Epsilon History.png')
+#     for i in range(n_games):
+#         done = False
+#         score = 0
+#         observation = env.reset()
+
+#         while not done:
+#             action = agent.choose_action(observation)
+#             next_observation, reward, done, info = env.step(action)
+#             score += reward
+#             agent.remember(observation, action, reward, next_observation, done)
+#             observation = next_observation
+#             agent.learn()
+
+#         eps_history.append(agent.epsilon)
+#         scores.append(score)
+
+#         avg_score = np.mean(scores[max(0, i - 100): i + 1])
+#         print('episode ', i, 'score %.2f' % score, 'average score %.2f' % avg_score)
+
+#         if i % 10 == 0 and i > 0:
+#             agent.save_model()
+
+#         plt.plot(scores)
+#         plt.savefig('Scores.png')
+#         plt.plot(eps_history)
+#         plt.savefig('Epsilon History.png')
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import gym
-env = gym.make('BipedalWalker-v3')
+
+# env = gym.make('BipedalWalker-v3')
+# env = gym.make('LunarLander-v2')
+# env = gym.make('CarRacing-v0')
+env = gym.make('CartPole-v1')
+
 env.reset()
+print(env.action_space.sample())
 for _ in range(1000):
     env.render()
     env.step(env.action_space.sample()) # take a random action
