a73d6b12923ee87c2eb1e2460717c09f926023af
Rishav Bhagat <rishavb123@gmail.com>
2020-04-03 00:02:21 -0400

    added a log statement

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import gym
 env = gym.make('CartPole-v1')
 
 env.reset()
-print(env.action_space.sample())
+print(env.action_space.sample(), len(env.get_state()))
 for _ in range(1000):
     env.render()
     env.step(env.action_space.sample()) # take a random action
