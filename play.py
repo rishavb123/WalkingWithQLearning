9d9531fdcd52a62421a9cd8de0dbe84987af6daf
Rishav Bhagat <rishavb123@gmail.com>
2020-04-05 15:15:22 -0400

    deleted useless code

--- play.py
+++ play.py
@@ -119,38 +119,3 @@ if __name__ == '__main__':
             env.render()
 
         print("Score was", score)
-
-# def train():
-#     env = gym.make('LunarLander-v2')
-#     n_games = 500
-#     agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.0005, input_dims=8, n_actions=4, mem_size=1000000, batch_size=64, epsilon_dec=0.999, epsilon_min=0.01)
-
-#     scores = []
-#     eps_history = []
-
-#     for i in range(n_games):
-#         done = False
-#         score = 0
-#         observation = env.reset()
-
-#         while not done:
-#             action = agent.choose_action(observation)
-#             next_observation, reward, done, info = env.step(action)
-#             score += reward
-#             agent.remember(observation, action, reward, next_observation, done)
-#             observation = next_observation
-#             agent.learn()
-
-#         eps_history.append(agent.epsilon)
-#         scores.append(score)
-
-#         avg_score = np.mean(scores[max(0, i - 100): i + 1])
-#         print('episode ', i, 'score %.2f' % score, 'average score %.2f' % avg_score)
-
-#         if i % 10 == 0 and i > 0:
-#             agent.save_model()
-
-#         plt.plot(scores)
-#         plt.savefig('Scores.png')
-#         plt.plot(eps_history)
-#         plt.savefig('Epsilon History.png')
